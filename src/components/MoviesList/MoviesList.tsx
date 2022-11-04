299196b676af54727c6ab72b8fcdbbef0e968b6d
VolodymyrKirichenko <109915550+VolodymyrKirichenko@users.noreply.github.com>
2022-11-04 20:11:53 +0200

    added my project (#1)
    * added my project
    
    * changed the acceptance of propsÑ

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,20 @@
-import React from 'react';
+import { FC } from 'react';
 import './MoviesList.scss';
+import { MovieCard } from '../MovieCard';
+import { Movie } from '../../types/Movie';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
-);
+interface Props {
+  movies: Movie[];
+}
+
+export const MoviesList: FC<Props> = (props) => {
+  const { movies } = props;
+
+  return (
+    <div className="movies">
+      {movies.map(movie => (
+        <MovieCard movie={movie} key={movie.imdbId} />
+      ))}
+    </div>
+  );
+};
